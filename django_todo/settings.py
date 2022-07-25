0f5ad9950b00e56f1d3e1e84cda08e53d1316d5f
Arron <beale86@msn.com>
2022-07-25 17:30:33 +0000

    Add todo_list.html, add get_todo_list function to views.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9*$5@4g@4lt54kwfvtq4gi=dik-ww@knwuu#zj8111tng+x!nf'
+SECRET_KEY = (
+    'django-insecure-9*$5@4g@4lt54kwfvtq4gi=dik-ww@knwuu#zj8111tng+x!nf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
