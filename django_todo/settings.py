397408b553f2c6edfa2cc8343d739e4d12926bc1
Arron <beale86@msn.com>
2022-07-28 18:30:31 +0000

    Setup environ variables and auto deployments

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = ['hello-django-todo-app-arron.herokuapp.com']
 
 
 # Application definition
